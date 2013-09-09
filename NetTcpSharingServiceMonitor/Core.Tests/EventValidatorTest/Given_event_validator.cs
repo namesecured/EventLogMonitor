a3710a6747fb79df16fca6c3fef3ea6b38e34dcf
Sergey Psekha <pss@ciklum.com>
2013-09-09 14:54:12 +0300

    EventValidator: added tests;

---
+++ NetTcpSharingServiceMonitor/Core.Tests/EventValidatorTest/Given_event_validator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Core.Tests.EventValidatorTest
+{
+    [TestFixture]
+    public abstract class Given_event_validator : TestBase
+    {
+        public IEventValidator EventValidator { get; set; }
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+        }
+    }
+}
